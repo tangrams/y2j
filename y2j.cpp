bf8d39033a6c6a7961ceeb26d184b31426f4b8c8
Matt Blair <blair1618@gmail.com>
2016-10-16 23:47:21 -0400

    Clean up 'Generator' struct

--- y2j.cpp
+++ y2j.cpp
@@ -1,10 +1,9 @@
 #include "y2j.h"
 #include "yaml.h"
-#include <string>
 #include <vector>
 
 #ifndef Y2J_DEBUG
-    #define Y2J_DEBUG 1
+#define Y2J_DEBUG 1
 #endif
 
 namespace y2j {
@@ -17,12 +16,26 @@ struct Collection {
 template<typename Handler>
 struct Generator {
 
+    Generator(const char* bytes, size_t length, const char** errorMessage, size_t* errorOffset) :
+        errorMessage(errorMessage),
+        errorOffset(errorOffset) {
+        yaml_parser_initialize(&parser);
+        yaml_parser_set_input_string(&parser, (const unsigned char*)bytes, length);
+        yaml_parser_set_encoding(&parser, YAML_UTF8_ENCODING);
+        yaml_stream_start_event_initialize(&event, YAML_UTF8_ENCODING);
+    }
+
+    ~Generator() {
+        yaml_event_delete(&event);
+        yaml_parser_delete(&parser);
+    }
+
     yaml_parser_t parser;
     yaml_event_t event;
     Collection collection;
     std::vector<Collection> collectionStack;
-    const char** errorMessage = nullptr;
-    size_t* errorOffset = nullptr;
+    const char** errorMessage;
+    size_t* errorOffset;
 
     size_t getSeqLength() {
         return collection.count;
@@ -50,10 +63,12 @@ struct Generator {
 
     bool operator()(Handler& handler) {
 
-        yaml_event_type_t type = YAML_NO_EVENT;
         bool ok = true;
 
-        do {
+        while (ok && event.type != YAML_STREAM_END_EVENT) {
+
+            yaml_event_delete(&event);
+
             if (!yaml_parser_parse(&parser, &event)) {
                 if (errorMessage) {
                     *errorMessage = parser.problem;
@@ -66,35 +81,29 @@ struct Generator {
             }
 
             #if Y2J_DEBUG
-            printEvent(event, parser.indent);
+            printEvent(event);
             #endif
 
-            type = event.type;
-
-            switch (type) {
+            switch (event.type) {
             case YAML_NO_EVENT:
-                break;
             case YAML_STREAM_START_EVENT:
-                break;
             case YAML_STREAM_END_EVENT:
-                break;
             case YAML_DOCUMENT_START_EVENT:
-                break;
             case YAML_DOCUMENT_END_EVENT:
                 break;
             case YAML_SEQUENCE_START_EVENT:
+                // FIXME: If a sequence or mapping is found in a key, add a string version of this node instead.
                 ok = handler.StartArray();
                 pushCollection(false);
-                // FIXME: If a sequence is found in a map key, add a key with a string-ified sequence instead.
                 break;
             case YAML_SEQUENCE_END_EVENT:
                 ok = handler.EndArray(getSeqLength());
                 popCollection();
                 break;
             case YAML_MAPPING_START_EVENT:
+                // FIXME: If a sequence or mapping is found in a key, add a string version of this node instead.
                 ok = handler.StartObject();
                 pushCollection(true);
-                // FIXME: If a mapping is found in a map key, add a key with a string-ified mapping instead.
                 break;
             case YAML_MAPPING_END_EVENT:
                 ok = handler.EndObject(getMapLength());
@@ -110,40 +119,32 @@ struct Generator {
                     ok = handler.String((char*)event.data.scalar.value, event.data.scalar.length, true);
                     // FIXME: Deduce types for integer, float, boolean, and null scalar values.
                 }
-                ++collection.count;
+                collection.count++;
                 break;
             }
-
-            yaml_event_delete(&event);
-
-            if (!ok) {
-                break;
-            }
-
-        } while (type != YAML_STREAM_END_EVENT);
-
-        yaml_event_delete(&event);
-        yaml_parser_delete(&parser);
+        }
 
         return ok;
     }
 
     #if Y2J_DEBUG
-    void printEvent(yaml_event_t& event, int indentSize) {
-        std::string indent(std::max(indentSize, 0), ' ');
+    void printEvent(yaml_event_t& event) {
+        static int indent = 0;
+        if (event.type == YAML_DOCUMENT_START_EVENT) { indent = 0; }
+        if (event.type == YAML_SEQUENCE_END_EVENT || event.type == YAML_MAPPING_END_EVENT) { indent -= 2; }
+        printf("%*s", indent, "");
         switch (event.type) {
         case YAML_NO_EVENT: printf("No event!\n"); break;
         case YAML_STREAM_START_EVENT: printf("Start Stream\n"); break;
         case YAML_STREAM_END_EVENT: printf("End Stream\n"); break;
-        case YAML_DOCUMENT_START_EVENT: printf("%s%s\n", indent.c_str(), "Start Document"); break;
-        case YAML_DOCUMENT_END_EVENT: printf("%s%s\n", indent.c_str(), "End Document"); break;
-        case YAML_SEQUENCE_START_EVENT: printf("%s[\n", indent.c_str()); break;
-        case YAML_SEQUENCE_END_EVENT: printf("%s] (members: %lu)\n", indent.c_str(), getSeqLength()); break;
-        case YAML_MAPPING_START_EVENT: printf("%s{\n", indent.c_str()); break;
-        case YAML_MAPPING_END_EVENT: printf("%s} (members: %lu)\n", indent.c_str(), getMapLength()); break;
-        case YAML_ALIAS_EVENT: printf("%sGot alias (anchor %s)\n", indent.c_str(), event.data.alias.anchor); break;
-        case YAML_SCALAR_EVENT: printf(entryIsMapKey() ? "%s\"%s\":\n" : "%s\"%s\"\n", indent.c_str(), event.data.scalar.value);
-            break;
+        case YAML_DOCUMENT_START_EVENT: printf("Start Document\n"); break;
+        case YAML_DOCUMENT_END_EVENT: printf("End Document\n"); break;
+        case YAML_SEQUENCE_START_EVENT: printf("[\n"); indent += 2; break;
+        case YAML_SEQUENCE_END_EVENT: printf("] (members: %lu)\n", getSeqLength()); break;
+        case YAML_MAPPING_START_EVENT: printf("{\n"); indent += 2; break;
+        case YAML_MAPPING_END_EVENT: printf("} (members: %lu)\n", getMapLength()); break;
+        case YAML_ALIAS_EVENT: printf("Alias (anchor %s)\n", event.data.alias.anchor); break;
+        case YAML_SCALAR_EVENT: printf(entryIsMapKey() ? "\"%s\":\n" : "\"%s\"\n", event.data.scalar.value); break;
         }
     }
     #endif
@@ -151,17 +152,9 @@ struct Generator {
 
 JsonDocument yamlParseBytes(const char* bytes, size_t length, const char** errorMessage, size_t* errorOffset) {
 
-    Generator<JsonDocument> generator;
-    generator.errorMessage = errorMessage;
-    generator.errorOffset = errorOffset;
-
-    yaml_parser_initialize(&generator.parser);
-    yaml_parser_set_input_string(&generator.parser, (const unsigned char*)bytes, length);
-    yaml_parser_set_encoding(&generator.parser, YAML_UTF8_ENCODING);
-
+    Generator<JsonDocument> generator(bytes, length, errorMessage, errorOffset);
     JsonDocument document;
     document.Populate(generator);
-
     return document;
 }
 
