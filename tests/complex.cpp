a987a424f0b28c458892a947f32ec408035a4bb4
Matt Blair <blair1618@gmail.com>
2016-11-29 16:01:29 -0500

    Simplified complex key tests

--- tests/complex.cpp
+++ tests/complex.cpp
@@ -5,11 +5,10 @@
 // http://www.yaml.org/spec/1.2/spec.html#id2760395
 
 static const char* source = R"END(
-? - Detroit Tigers
-  - Chicago cubs
-: - 2001-07-23
-New York Yankees: [ 2001-07-02, 2001-08-12, 2001-08-14 ]
-Atlanta Braves : [ 2001-07-02, 2001-08-12, 2001-08-14 ]
+? - complex: key
+: - value
+[ another, complex, key ]: [ more, values ]
+simple key: [ even, more, values ]
 )END";
 
 TEST_CASE("Complex mapping keys produce an 'error' node but don't halt parsing")
@@ -23,11 +22,7 @@ TEST_CASE("Complex mapping keys produce an 'error' node but don't halt parsing")
     CHECK(document.IsObject());
     CHECK(document.MemberCount() == 3);
 
-    auto& value0 = document["New York Yankees"];
-    CHECK(value0.IsArray());
-    CHECK(value0.Size() == 3);
-
-    auto& value1 = document["Atlanta Braves"];
-    CHECK(value1.IsArray());
-    CHECK(value1.Size() == 3);
+    auto& value = document["simple key"];
+    CHECK(value.IsArray());
+    CHECK(value.Size() == 3);
 }
